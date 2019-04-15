84663fa072aa4d4aeb8bfb39a3d625bf04fa9068
bluexD <ingrid.sas26@gmail.com>
2019-04-15 16:49:38 +0300

    Integration Tests & WBT

--- src/test/java/IngridOana/AppTest.java
+++ src/test/java/IngridOana/AppTest.java
@@ -1,18 +1,38 @@
 package IngridOana;
 
-import static org.junit.Assert.assertTrue;
-
+import domain.Student;
+import domain.Tema;
+import org.junit.Before;
 import org.junit.Test;
+import repository.StudentRepository;
+import repository.TemaRepository;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+    private StudentRepository studentRepository;
+    private TemaRepository temaRepository;
+
+    @Before
+    public void setUp() {
+        studentRepository = new StudentRepository(new StudentValidator());
+        temaRepository = new TemaRepository(new TemaValidator());
+    }
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void addStudent() {
+    public void addGrade() {
         assertTrue(true);
     }
 
@@ -22,9 +42,120 @@ public class AppTest {
     }
 
     @Test
-    public void addGrade() {
-        assertTrue(true);
+    public void addStudent() {
+        List<Student> students = StreamSupport.stream(studentRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(students.size(), 0);
+        studentRepository.save(new Student("1", "Oanaaaa", 936));
+        students = StreamSupport.stream(studentRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(students.size(), 1);
+        Student student = studentRepository.findOne("1");
+        assertEquals(student.getNume(), "Oanaaaa");
+        assertEquals(student.getGrupa(), 936);
+    }
+
+    @Test
+    public void shouldFailForNullId() {
+        try {
+            studentRepository.findOne(null);
+            fail();
+        } catch (Exception e) {
+            assertEquals("ID-ul nu poate fi null!", e.getMessage().trim());
+
+        }
+    }
+
+    @Test
+    public void shouldFailInvalidGroup() {
+        Student student = studentRepository.save(new Student("123", "lala", 1000));
+        assertEquals(student, null);
+    }
+
+    @Test
+    public void shouldFailInvalidGroupNegative() {
+        Student student = studentRepository.save(new Student("123", "lala", -100));
+        assertEquals(student, null);
+    }
+
+    @Test
+    public void shouldFailInvalidName() {
+        Student student = studentRepository.save(new Student("123", "", 310));
+        assertEquals(student, null);
+    }
+
+    @Test
+    public void shouldFailInvalidId() {
+        Student student = studentRepository.save(new Student("", "Ingriduta", 3100));
+        assertEquals(student, null);
     }
 
 
+    @Test
+    public void shouldAddAssignment() {
+        List<Tema> homeworks = StreamSupport.stream(temaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(homeworks.size(), 0);
+        temaRepository.save(new Tema("1", "Oanaaaa", 3, 1));
+        homeworks = StreamSupport.stream(temaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(homeworks.size(), 1);
+    }
+
+    @Test
+    public void shouldFailNullId() {
+        Tema temaIdEmpty = temaRepository.save(new Tema(null, "Ingriduta", 3, 2));
+        assertEquals(temaIdEmpty, null);
+    }
+
+    @Test
+    public void shouldFailEmptyId() {
+        Tema temaIdEmpty = temaRepository.save(new Tema("", "Ingriduta", 3, 2));
+        assertEquals(temaIdEmpty, null);
+    }
+
+
+    @Test
+    public void shouldFailNullDescription() {
+        Tema temaDescEmpty = temaRepository.save(new Tema("12", null, 3, 2));
+        assertEquals(temaDescEmpty, null);
+    }
+
+    @Test
+    public void shouldFailEmptyDescription() {
+        Tema temaDescEmpty = temaRepository.save(new Tema("12", "", 3, 2));
+        assertEquals(temaDescEmpty, null);
+    }
+
+    @Test
+    public void shouldFailNegativeDeadline() {
+        Tema negativeDeadline = temaRepository.save(new Tema("12", "desc", -1, 2));
+        assertEquals(negativeDeadline, null);
+    }
+
+    @Test
+    public void shouldFailDeadlineTooBig() {
+        Tema deadlineTooBig = temaRepository.save(new Tema("17", "desc", 19, 2));
+        assertEquals(deadlineTooBig, null);
+    }
+
+    @Test
+    public void shouldFailDeadlineSmallerThanStart() {
+        Tema deadlineSmaller = temaRepository.save(new Tema("17", "desc", 3, 5));
+        assertEquals(deadlineSmaller, null);
+    }
+
+    @Test
+    public void shouldFailStartLineNegative() {
+        Tema starlineNegative = temaRepository.save(new Tema("12", "desc", 4, -4));
+        assertEquals(starlineNegative, null);
+    }
+
+    @Test
+    public void shouldFailStartlineTooBig() {
+        Tema startLineTooBig = temaRepository.save(new Tema("17", "desc", 3, 25));
+        assertEquals(startLineTooBig, null);
+    }
+
+    @Test
+    public void shouldFailStartlineGreaterThanDeadline() {
+        Tema startLineTooBig = temaRepository.save(new Tema("17", "desc", 3, 25));
+        assertEquals(startLineTooBig, null);
+    }
 }
---
+++ src/test/java/IngridOana/BigBangTests.java
@@ -0,0 +1,111 @@
+package IngridOana;
+
+import domain.Nota;
+import domain.Pair;
+import domain.Student;
+import domain.Tema;
+import org.junit.Before;
+import org.junit.Test;
+import repository.*;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BigBangTests {
+    private StudentRepository studentRepository;
+    private TemaRepository temaRepository;
+    private NotaRepository notaRepository;
+    private Service service;
+
+
+    @Before
+    public void setUp() {
+        studentRepository = new StudentRepository(new StudentValidator());
+        temaRepository = new TemaRepository(new TemaValidator());
+        notaRepository = new NotaRepository(new NotaValidator());
+
+        //
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(new StudentValidator(), "LabAssiAsseProjectV01\\studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(new TemaValidator(), "LabAssiAsseProjectV01\\teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(new NotaValidator(), "LabAssiAsseProjectV01\\note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    public void addStudent() {
+        List<Student> students = StreamSupport.stream(studentRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(students.size(), 0);
+        studentRepository.save(new Student("1", "Oanaaaa", 936));
+        students = StreamSupport.stream(studentRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(students.size(), 1);
+    }
+
+    @Test
+    public void shouldAddAssignment() {
+        List<Tema> homeworks = StreamSupport.stream(temaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(homeworks.size(), 0);
+        temaRepository.save(new Tema("1", "Oanaaaa", 3, 1));
+        homeworks = StreamSupport.stream(temaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(homeworks.size(), 1);
+        Tema tema = temaRepository.findOne("1");
+        assertNotNull(tema.toString(), "");
+    }
+
+    @Test
+    public void shouldAddGrade() {
+        List<Nota> note = StreamSupport.stream(notaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(note.size(), 0);
+        notaRepository.save(new Nota(new Pair<>("1", "nota1"), 7.5, 3, "Smart Girl"));
+        note = StreamSupport.stream(notaRepository.findAll().spliterator(), false).collect(Collectors.toList());
+        assertEquals(note.size(), 1);
+    }
+
+    @Test
+    public void shouldAddStudent() {
+        List<Student> studenti = StreamSupport.stream(service.findAllStudents().spliterator(), false).collect(Collectors.toList());
+        assertEquals(studenti.size(), 4);
+        service.saveStudent("10", "Oana", 936);
+        assertEquals(studenti.size() + 1, 5);
+    }
+
+    @Test
+    public void shouldAddNewGrade() {
+        List<Nota> note = StreamSupport.stream(service.findAllNote().spliterator(), false).collect(Collectors.toList());
+        assertEquals(note.size(), 2);
+        service.saveNota("10", "3", 10.0, 1, "Oana este frumoasa si desteapta");
+        assertEquals(note.size() + 1, 3);
+    }
+
+    @Test
+    public void shouldFailAddNewGrade() {
+        List<Nota> note = StreamSupport.stream(service.findAllNote().spliterator(), false).collect(Collectors.toList());
+        assertEquals(note.size(), 2);
+        assertEquals(-1, service.saveNota("5", "3", 10.0, 1, "Oana este frumoasa si desteapta"));
+        assertEquals(note.size(), 2);
+    }
+
+    @Test
+    public void shouldAddAssignement() {
+        List<Tema> teme = StreamSupport.stream(service.findAllTeme().spliterator(), false).collect(Collectors.toList());
+        assertEquals(teme.size(), 4);
+        service.saveTema("4", "Sth", 4, 2);
+        assertEquals(teme.size() + 1, 5);
+    }
+
+    @Test
+    public void bigBang() {
+        shouldAddStudent();
+        shouldAddNewGrade();
+        shouldAddAssignement();
+    }
+}
